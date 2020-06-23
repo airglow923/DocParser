8615e9d00350b45165664992f046744e6796b55d
airglow923 <p.hyundeok76@gmail.com>
2020-06-23 20:12:59 +0800

    conditional FibRgFcLcb

--- src/Fib/FibRgFcLcb/FibRgFcLcb.h
+++ src/Fib/FibRgFcLcb/FibRgFcLcb.h
@@ -9,4 +9,18 @@
 #include "FibRgFcLcb2003.h"
 #include "FibRgFcLcb2007.h"
 
+#if defined(NFIB)
+    #if NFIB == 0x00C1
+        #define FibRgFcLcb FibRgFcLcb97
+    #elif NFIB == 0x00D9
+        #define FibRgFcLcb FibRgFcLcb2000
+    #elif NFIB == 0x0101
+        #define FibRgFcLcb FibRgFcLcb2002
+    #elif NFIB == 0x010C
+        #define FibRgFcLcb FibRgFcLcb2003
+    #elif NFIB == 0x0112
+        #define FibRgFcLcb FibRgFcLcb2007
+    #endif
+#endif
+
 #endif
