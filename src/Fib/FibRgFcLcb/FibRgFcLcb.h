c0f78f88a27fcb1bb1d3170e90fb3ccd7bd3527b
airglow923 <p.hyundeok76@gmail.com>
2020-06-24 11:28:25 +0800

    indentation

--- src/Fib/FibRgFcLcb/FibRgFcLcb.h
+++ src/Fib/FibRgFcLcb/FibRgFcLcb.h
@@ -10,17 +10,19 @@
 #include "FibRgFcLcb2007.h"
 
 #ifdef NFIB
-    #if NFIB == 0x00C1
-        #define FibRgFcLcb FibRgFcLcb97
-    #elif NFIB == 0x00D9
-        #define FibRgFcLcb FibRgFcLcb2000
-    #elif NFIB == 0x0101
-        #define FibRgFcLcb FibRgFcLcb2002
-    #elif NFIB == 0x010C
-        #define FibRgFcLcb FibRgFcLcb2003
-    #elif NFIB == 0x0112
-        #define FibRgFcLcb FibRgFcLcb2007
-    #endif
+
+#if NFIB == 0x00C1
+    #define FibRgFcLcb FibRgFcLcb97
+#elif NFIB == 0x00D9
+    #define FibRgFcLcb FibRgFcLcb2000
+#elif NFIB == 0x0101
+    #define FibRgFcLcb FibRgFcLcb2002
+#elif NFIB == 0x010C
+    #define FibRgFcLcb FibRgFcLcb2003
+#elif NFIB == 0x0112
+    #define FibRgFcLcb FibRgFcLcb2007
+#endif
+
 #endif
 
 #endif
