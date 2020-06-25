f5dba5601ba5f527cc42b6123fa9fcd6ee95b5b2
airglow923 <p.hyundeok76@gmail.com>
2020-06-25 10:39:42 +0800

    indentation

--- src/Fib/Fib.h
+++ src/Fib/Fib.h
@@ -24,13 +24,13 @@
 
 typedef struct {
     unsigned int base: SZ_FIBBASE * 8;
-    unsigned int csw:  16;
+    unsigned int csw: 16;
     unsigned int fibRgW: SZ_FIBRGW97 * 8;
-    unsigned int cslw:  16;
+    unsigned int cslw: 16;
     unsigned int fibRgLw: SZ_FIBRGLW97 * 8;
-    unsigned int cbRgFcLcb:  16;
+    unsigned int cbRgFcLcb: 16;
     FibRgFcLcb fibRgFcLcbBlob;
-    unsigned int cswNew:  16;
+    unsigned int cswNew: 16;
     FibRgCswNew fibRgCswNew;
 } Fib;
 
