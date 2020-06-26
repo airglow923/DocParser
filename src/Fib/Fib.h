20dbd5e20216e0dcf87aeed2af2c5ec1a1c9a12f
airglow923 <p.hyundeok76@gmail.com>
2020-06-26 15:17:57 +0800

    remove unnecessary comments

--- src/Fib/Fib.h
+++ src/Fib/Fib.h
@@ -9,19 +9,6 @@
 #include "FibRgFcLcb/FibRgFcLcb.h"
 #include "FibRgCswNew/FibRgCswNew.h"
 
-/*
- * The Fib structure contains information about the document and specifies the
- * file pointers to various portions that make up the document.
- * 
- * The Fib is a variable length structure. With the exception of the base
- * portion which is fixed in size, every section is preceded with a count field
- * that specifies the size of the next section.
- * 
- * References from Microsoft:
- * Fib: https://docs.microsoft.com/en-us/openspecs/office_file_formats/ms-doc/9aeaa2e7-4a45-468e-ab13-3f6193eb9394
- * FibBase: https://docs.microsoft.com/en-us/openspecs/office_file_formats/ms-doc/26fb6c06-4e5c-4778-ab4e-edbf26a545bb
- */
-
 typedef struct {
     unsigned int base: SZ_FIBBASE * 8;
     unsigned int csw: 16;
