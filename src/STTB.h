e5722f089fe9038c5f6624560744a496536d8619
airglow923 <p.hyundeok76@gmail.com>
2020-07-03 20:55:40 +0800

    done STTB.h

--- src/STTB.h
+++ src/STTB.h
@@ -4,6 +4,21 @@
 #define __STTB_H__
 
 typedef struct STTB {
+    unsigned int fExtend: 16;
+    union {
+        unsigned int fExtendNotExists: 8;
+        unsigned int fExtendExists: 16;
+    } cData;
+    unsigned int cbExtra: 16;
+    union {
+        unsigned int fExtendNotExists: 8;
+        unsigned int fExtendExists: 16;
+    } cchData;
+    union {
+        unsigned int fExtendNotExists: 8;
+        unsigned int fExtendExists: 32;
+    } cchData;
+    unsigned int extraData: 16;
 } STTB;
 
 #endif
