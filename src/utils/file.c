e033bb7e795838a8159e8265cf22880dcd876826
airglow923 <p.hyundeok76@gmail.com>
2020-08-07 21:26:13 +0800

    merge utilities files

---
+++ src/utils/file.c
@@ -0,0 +1,48 @@
+#include "file.h"
+
+int is_big_endian(void)
+{
+    int i = 0x1234;
+    char *ch = (char *) &i;
+    return *ch == 0x12;
+}
+
+int is_small_endian(void)
+{
+    int i = 0x1234;
+    char *ch = (char *) &i;
+    return *ch == 0x34;
+}
+
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
+
+LONGLONG get_file_size(const WINCHAR *filename)
+{
+    HANDLE hFile = CreateFile(filename, GENERIC_READ,
+        FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if (hFile == INVALID_HANDLE_VALUE)
+        return -1;
+
+    LARGE_INTEGER size;
+
+    if (!GetFileSizeEx(hFile, &size)) {
+        CloseHandle(hFile);
+        return -1;
+    }
+
+    CloseHandle(hFile);
+    return size.QuadPart;
+}
+
+#elif defined(__unix__) || defined(__linux__)
+
+off_t get_file_size(const char *filename)
+{
+    struct stat st;
+    stat(filename, &st);
+    return st.st_size;
+}
+
+#endif
---
+++ src/utils/file.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#ifndef __FILE_H__
+#define __FILE_H__
+
+/* header inclusion and macro definition */
+
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
+
+#define UNICODE
+
+#include <fileapi.h>
+#include <handleapi.h>
+#include <inttypes.h>
+
+#ifdef UNICODE
+typedef wchar_t WINCHAR
+#else
+typedef char WINCHAR
+#endif
+
+#elif defined(unix)
+    || defined(__unix)
+    || defined(__unix__)
+    || defined(__linux__)
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#endif
+
+/* function declaration */
+
+int is_big_endian(void);
+int is_little_endian(void);
+
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
+
+LONGLONG get_file_size(const WINCHAR *filename);
+
+#elif defined(unix)
+    || defined(__unix)
+    || defined(__unix__)
+    || defined(__linux__)
+
+off_t get_file_size(const char *filename);
+
+#endif
+
+#endif
