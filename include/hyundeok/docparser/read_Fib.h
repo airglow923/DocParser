ae713bc0e5f3bc1172e4408002e293ceb961b1d1
airglow923 <p.hyundeok76@gmail.com>
2021-01-21 05:34:58 +0800

    replace flie pointer with HyundeokHeap

--- include/hyundeok/docparser/read_Fib.h
+++ include/hyundeok/docparser/read_Fib.h
@@ -1,19 +1,14 @@
 #ifndef HYUNDEOK_DOCPARSER_READ_FIB_H
 #define HYUNDEOK_DOCPARSER_READ_FIB_H
 
+#include "hyundeok/HyundeokHeap.h"
 #include "hyundeok/docparser/msdoc/Fib/Fib.h"
-#include "hyundeok/docparser/msdoc/Fib/FibBase.h"
-#include "hyundeok/docparser/msdoc/Fib/FibRgCswNew/FibRgCswNew.h"
-
-#include <stdio.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int read_FibBase(FibBase* restrict fibBase, FILE* restrict fp);
-int read_FibRgCswNew(FibRgCswNew* restrict fibRgCswNew, FILE* restrict fp);
-int read_Fib(Fib* restrict fib, FILE* restrict fp);
+int read_Fib(Fib* restrict dest, HyundeokHeap* restrict src);
 
 #ifdef __cplusplus
 }
--- src/docparser/read_Fib.c
+++ src/docparser/read_Fib.c
@@ -1,35 +1,40 @@
 #include "hyundeok/docparser/read_Fib.h"
 
 #include "hyundeok/macro.h"
+#include "hyundeok/docparser/msdoc/Fib/FibBase.h"
+#include "hyundeok/docparser/msdoc/Fib/FibRgCswNew/FibRgCswNew.h"
 
-#include <string.h> // memset
+#include <string.h>   // memcpy
 
-int read_FibBase(FibBase* restrict fibBase, FILE* restrict fp) {
-  fread(fibBase, sizeof(FibBase), 1, fp);
+static int read_FibBase(FibBase* restrict dest, void* restrict src) {
+  *dest = *((FibBase*) src);
 
-  HYUNDEOK_ASSERT(fibBase->wident, 0xA5EC, -1,);
+  HYUNDEOK_ASSERT(dest->wident, 0xA5EC, -1, );
 
   // TODO: handle lid
   // TODO: handle pnNext
 
-  HYUNDEOK_ASSERT(fibBase->envr, 0, -1,);
-  HYUNDEOK_ASSERT(fibBase->fMac, 0, -1,);
-  HYUNDEOK_ASSERT(fibBase->fEmptySpecial, 0, -1,);
+  HYUNDEOK_ASSERT(dest->envr, 0, -1, );
+  HYUNDEOK_ASSERT(dest->fMac, 0, -1, );
+  HYUNDEOK_ASSERT(dest->fEmptySpecial, 0, -1, );
 
   return 0;
 }
 
-int read_FibRgCswNew(FibRgCswNew* restrict fibRgCswNew, FILE* restrict fp) {
-  fread(&fibRgCswNew->nFibNew, sizeof(fibRgCswNew->nFibNew), 1, fp);
+static int read_FibRgCswNew(FibRgCswNew* restrict dest, void* restrict src) {
+  FibRgCswNew* copy = src;
 
-  switch (fibRgCswNew->nFibNew) {
+  dest->nFibNew = copy->nFibNew;
+
+  switch (copy->nFibNew) {
   case 0x00D9:
   case 0x0101:
   case 0x010C:
-    fread(&fibRgCswNew->rgCswNewData, sizeof(FibRgCswNewData2000), 1, fp);
+    memcpy(&dest->rgCswNewData, &copy->rgCswNewData, sizeof(FibRgCswNewData2000));
     break;
   case 0x0112:
-    fread(&fibRgCswNew->rgCswNewData, sizeof(FibRgCswNewData2007), 1, fp);
+    memcpy(&dest->rgCswNewData, &copy->rgCswNewData, sizeof(FibRgCswNewData2007));
+    break;
     break;
   default:
     HYUNDEOK_PRTN_ERR("Invalid FibRgCswNew.nFibNew\n");
@@ -39,62 +44,63 @@ int read_FibRgCswNew(FibRgCswNew* restrict fibRgCswNew, FILE* restrict fp) {
   return 0;
 }
 
-int read_Fib(Fib* restrict fib, FILE* restrict fp) {
-  // set fib to 0
-  memset(fib, 0, sizeof(Fib));
+int read_Fib(Fib* restrict dest, HyundeokHeap* restrict src) {
+  // check whether the size of heap equals that of Fib
+  HYUNDEOK_ASSERT(src->size, sizeof(Fib), -1, );
 
-  HYUNDEOK_ASSERT(read_FibBase(&fib->base, fp), 0, -1,);
+  Fib* copy = src->data;
 
-  fread(&fib->csw, sizeof(fib->csw), 1, fp);
-  HYUNDEOK_ASSERT(fib->csw, 0x000E, -1,);
+  HYUNDEOK_ASSERT(read_FibBase(&dest->base, &copy->base), 0, -1, );
+
+  HYUNDEOK_ASSERT(copy->csw, 0x000E, -1, );
+  dest->csw = copy->csw;
 
   // TODO: read minimum of Fib.csw * 2 bytes
-  fread(&fib->fibRgW, sizeof(FibRgW97), 1, fp);
+  dest->fibRgW = copy->fibRgW;
 
-  fread(&fib->cslw, sizeof(fib->cslw), 1, fp);
-  HYUNDEOK_ASSERT(fib->cslw, 0x0016, -1,);
+  HYUNDEOK_ASSERT(copy->cslw, 0x0016, -1, );
+  dest->cslw = copy->cslw;
 
   // TODO: read minimum of Fib.cslw * 4 bytes
-  fread(&fib->fibRgLw, sizeof(FibRgLw97), 1, fp);
+  dest->fibRgLw = copy->fibRgLw;
 
   size_t sizeof_FibRgFcLcb;
 
   // NOLINTNEXTLINE(bugprone-narrowing-conversions)
-  switch (fib->base.nFib) {
+  switch (copy->base.nFib) {
   case 0x00C1:
-    fib->cbRgFcLcb = 0x005D;
+    dest->cbRgFcLcb = 0x005D;
     sizeof_FibRgFcLcb = sizeof(FibRgFcLcb97);
-    fib->cswNew = 0;
+    dest->cswNew = 0;
     break;
   case 0x00D9:
-    fib->cbRgFcLcb = 0x006C;
+    dest->cbRgFcLcb = 0x006C;
     sizeof_FibRgFcLcb = sizeof(FibRgFcLcb2000);
-    fib->cswNew = 0x0002;
+    dest->cswNew = 0x0002;
     break;
   case 0x0101:
-    fib->cbRgFcLcb = 0x0088;
+    dest->cbRgFcLcb = 0x0088;
     sizeof_FibRgFcLcb = sizeof(FibRgFcLcb2002);
-    fib->cswNew = 0x0002;
+    dest->cswNew = 0x0002;
     break;
   case 0x010C:
-    fib->cbRgFcLcb = 0x00A4;
+    dest->cbRgFcLcb = 0x00A4;
     sizeof_FibRgFcLcb = sizeof(FibRgFcLcb2003);
-    fib->cswNew = 0x0002;
+    dest->cswNew = 0x0002;
     break;
   case 0x0112:
-    fib->cbRgFcLcb = 0x00B7;
+    dest->cbRgFcLcb = 0x00B7;
     sizeof_FibRgFcLcb = sizeof(FibRgFcLcb2007);
-    fib->cswNew = 0x0005;
+    dest->cswNew = 0x0005;
     break;
   }
 
   // TODO: read minimum of Fib.cbRgFcLcb * 8 bytes
-  fread(&fib->fibRgFcLcbBlob, sizeof_FibRgFcLcb, 1, fp);
-  // NOLINTNEXTLINE(bugprone-narrowing-conversions)
-  fseek(fp, sizeof(fib->fibRgFcLcbBlob) - sizeof_FibRgFcLcb, SEEK_CUR);
+  dest->fibRgFcLcbBlob = copy->fibRgFcLcbBlob;
 
   // TODO: read minimum of Fib.cbRgFcLcb * 8 bytes
-  HYUNDEOK_ASSERT(read_FibRgCswNew(&fib->fibRgCswNew, fp), 0, -1,);
+  HYUNDEOK_ASSERT(read_FibRgCswNew(&dest->fibRgCswNew, &copy->fibRgCswNew), 0,
+                  -1, );
 
   return 0;
 }
