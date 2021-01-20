e09b737b05585f84231bc22c8b3279df8d9f42e5
airglow923 <p.hyundeok76@gmail.com>
2021-01-20 17:18:21 +0800

    apply new macros

--- src/utils/file.c
+++ src/utils/file.c
@@ -1,5 +1,9 @@
 #include "hyundeok/docparser/utils/file.h"
 
+#include "hyundeok/macro.h"
+
+#include <stdlib.h>
+
 int is_big_endian(void) {
   int i = 0x1234;
   char* ch = (char*)&i;
@@ -10,6 +14,25 @@ int is_small_endian(void) {
   int i = 0x1234;
   char* ch = (char*)&i;
   return *ch == 0x34;
+}
+
+HyundeokHeap* copy_struct_from_file(FILE* fp, size_t size) {
+  HyundeokHeap* heap = malloc(sizeof(HyundeokHeap));
+  HYUNDEOK_MEMORY_ASSERT(heap,);
+
+  heap->data = malloc(size);
+
+  // if allocation of heap->data fails, deallocate heap
+  HYUNDEOK_MEMORY_ASSERT(heap, free(heap));
+
+  heap->size = size;
+
+  const size_t res = fread(heap->data, size, 1, fp);
+
+  // if fread fails, deallocate heap->data and heap
+  HYUNDEOK_ASSERT(res, 1, NULL, free(heap->data); free(heap));
+
+  return heap;
 }
 
 #if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
