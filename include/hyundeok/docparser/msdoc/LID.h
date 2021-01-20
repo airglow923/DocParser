973498f739856d809d556f906167b1ae59bda717
airglow923 <p.hyundeok76@gmail.com>
2021-01-20 11:36:25 +0800

    add LID

---
+++ include/hyundeok/docparser/msdoc/LID.h
@@ -0,0 +1,16 @@
+#ifndef HYUNDEOK_DOCPARSER_OPENXML_LID_H
+#define HYUNDEOK_DOCPARSER_OPENXML_LID_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct LID {
+  unsigned int: 16;
+} LID;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
